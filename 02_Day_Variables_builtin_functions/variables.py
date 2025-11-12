0ee419e408fe53f6180b5ddca79c0cee1e8a906c
Luke Devlin <lukedevlin45@gmail.com>
2025-11-12 22:04:05 +0000

    actual day 2 completion

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -25,3 +25,26 @@ print(type(os))
 
 print(len(first_name))
 
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_two*num_one
+divsion=num_one/num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+def details():
+    first = input("What is your first name?")
+    last = input("What is your last name?")
+    country = input("Where are you from?")
+    age = input("What age are you?")
+    return [first,last,country,age]
+
+answer = input("Can I take your details [y/n]?")
+if answer == "y":
+    details()
+elif answer == "n":
+    pass
